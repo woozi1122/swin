c9ae4e64634000b69710efbe19e4ab049eff9629
woozi1122 <87755876+woozi1122@users.noreply.github.com>
2021-11-19 18:32:53 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -20,7 +20,7 @@ def main():
          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
     # load image
-    img_path = "../tulip.jpg"
+    img_path = "/kaggle/input/flowers-recognition/flowers/tulip/100930342_92e8746431_n.jpg"
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
     img = Image.open(img_path)
     plt.imshow(img)
